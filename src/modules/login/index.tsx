5f5b71143e9dd40c0264a0dfb7af85801e118fa2
Ryan Harman <ryan.harman63@gmail.com>
2023-05-04 18:14:14 +0100

    Remove unused code in login module

--- src/modules/login/index.tsx
+++ src/modules/login/index.tsx
@@ -18,15 +18,6 @@ export function Login() {
   return (
     <>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <Link
-          href="/examples/authentication"
-          className={cn(
-            buttonVariants({ variant: "ghost", size: "sm" }),
-            "absolute right-4 top-4 md:right-8 md:top-8"
-          )}
-        >
-          Login
-        </Link>
         <div className="relative hidden h-screen flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div
             className="absolute inset-0 bg-cover"
@@ -63,7 +54,7 @@ export function Login() {
               )}{" "}
               Github
             </Button>
-            <p className="px-8 text-center text-sm text-muted-foreground">
+            {/* <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{" "}
               <Link
                 href="/terms"
@@ -79,7 +70,7 @@ export function Login() {
                 Privacy Policy
               </Link>
               .
-            </p>
+            </p> */}
           </div>
         </div>
       </div>
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,8 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
 import type { NextPage } from "next";
-import { signIn } from "next-auth/react";
 import Head from "next/head";
 import { Login } from "~/modules/login";
-import { Button } from "~/modules/ui";
 
 const LoginPage: NextPage = () => {
-  async function handleLogin() {
-    await signIn("discord", { callbackUrl: "http://localhost:3000" });
-  }
-
   return (
     <>
       <Head>
